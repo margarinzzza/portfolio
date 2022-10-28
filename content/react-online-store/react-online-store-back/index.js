a203c40672bb4137f356ee400cf992c0d115ea01
margarinzzza <108409558+margarinzzza@users.noreply.github.com>
2022-10-28 20:19:58 +0600

    online-store v0

---
+++ content/react-online-store/react-online-store-back/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const cors = require('cors')
+const routes = require('./routes.js')
+const express = require('express')
+const models = require('./models/models.js')
+const sequelize = require('./db.js')
+const PORT = process.env.PORT || 4000
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/', routes);
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, ()=>console.log(`server start on port: ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    } 
+}
+
+start()
