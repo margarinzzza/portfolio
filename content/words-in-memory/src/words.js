18c83009f066c5a96e3daa8d8490708b2ff57ba4
margarinzzza <108409558+margarinzzza@users.noreply.github.com>
2022-11-17 16:03:55 +0600

    1

---
+++ content/words-in-memory/src/words.js
@@ -0,0 +1,22 @@
+export const wordsArr = [
+    'wednesday', 'February', 'August', 'cheque', 'withdraw', 'architecture', 'geography', 'research',
+    'review', 'specialist', 'knowledge', 'thistle', 'clothes', 'illegitimate', 'invulnerability', 'employer', 
+    'prejudice', 'hierarchical', 'entrepreneurial', 'appointment', 'confidence', 'government', 'achievable', 'performance',
+    'opportunity', 'persuade', 'exaggerate', 'success', 'accommodation', 'embarrass', 'millennium', 'wheelchair',
+    'vineyard', 'dragonfly', 'quicksand', 'telltale', 'attorneygeneral', 'haircut', 'whitewashing', 'onlooker',
+    'ticketinspector', 'breakthrough'
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
