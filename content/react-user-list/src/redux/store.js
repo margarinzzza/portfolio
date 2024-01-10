e33f129b22e6b0eae0332e72c8bf92f4d6476542
margarinzzza <108409558+margarinzzza@users.noreply.github.com>
2024-01-10 15:33:24 +0600

    Delete content/react-user-list directory

--- content/react-user-list/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import userListReducer from './slices/userListSlice'
-
-export const store = configureStore({
-  reducer: {
-    userList: userListReducer,
-  }
-})
-
