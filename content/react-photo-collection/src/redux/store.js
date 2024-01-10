116731184dba87d507a75089c42db59409e46b8a
margarinzzza <108409558+margarinzzza@users.noreply.github.com>
2024-01-10 15:33:05 +0600

    Delete content/react-photo-collection directory

--- content/react-photo-collection/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import photoCollectionsReducer from './slices/photoCollectionsSlice'
-
-export const store = configureStore({
-  reducer: {
-    photoCollections: photoCollectionsReducer,
-  }
-})
-
